446cfebc91871cb6c000caf1856ead37a4def22b
GPS-02 <root1@gps-02s-imac.local>
2018-08-20 11:29:48 -0300

    First Version

---
+++ TodoList/TodoList.swift
@@ -0,0 +1,13 @@
+//
+//  TodoList.swift
+//  TodoList
+//
+//  Created by GPS-02 on 20/08/2018.
+//  Copyright © 2018 Lucas Botelho. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class
